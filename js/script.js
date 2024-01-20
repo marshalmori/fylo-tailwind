cf5488a3bb91d3ff8c0460c3ea2455b0fea4a06f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-01-19 23:04:09 -0300

    Hero Section

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-// const themeToggleBtn = document.getElementById("theme-toggle");
-// const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-// const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
-/*
 if (
-  localStorage.getItem('color-theme') === 'dark' ||
-  (!('color-theme' in localStorage) &&
-  window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ) {
-    // Show light icon
-    themeToggleLightIcon.classList.remove('hidden')
-  } else {
-    themeToggleDarkIcon.classList.remove('hidden')
-  }
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  // Show light icon
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
 
-  // Listen for toggle button click
-  themeToggleBtn.addEventListener('click', toggleMode)
-  */
+// Listen for toggle button click
+themeToggleBtn.addEventListener("click", toggleMode);
 
 function toggleMode() {
   // Toggle icon
